9d0c6f2bd0afefa2f4f901b43b9372503a23eb90
Gustavo.Marcolla <gustavo.marcolla@senior.com.br>
2025-07-30 13:54:43 -0300

    Initial commit app

---
+++ ConnectAppsChat/Network.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectAppsChat
+{
+    public class Network
+    {
+        private int[] parent;
+
+        public Network(int size)
+        {
+            if (size <= 0)
+                throw new ArgumentException("Size must be a positive integer");
+
+            parent = new int[size + 1]; // +1 para facilitar indexação de 1 até size
+
+            for (int i = 1; i <= size; i++)
+            {
+                parent[i] = i;
+            }
+
+        }
+
+        private int Find(int x)
+        {
+            if (parent[x] != x)
+                parent[x] = Find(parent[x]); // Path compression
+            return parent[x];
+        }
+
+        public void Connect(int a, int b)
+        {
+            Validate(a);
+            Validate(b);
+            int rootA = Find(a);
+            int rootB = Find(b);
+            if (rootA != rootB)
+                parent[rootA] = rootB;
+        }
+
+        public bool Query(int a, int b)
+        {
+            Validate(a);
+            Validate(b);
+            return Find(a) == Find(b);
+        }
+
+        private void Validate(int x)
+        {
+            if (x <= 0 || x >= parent.Length)
+                throw new ArgumentOutOfRangeException($"Element {x} is out of range");
+        }
+    }
+}
---
+++ ConnectAppsChat/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConnectAppsChat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var network = new Network(8);
+            network.Connect(1, 2);
+            network.Connect(6, 2);
+            network.Connect(2, 4);
+            network.Connect(5, 8);
+
+            
+            Console.WriteLine(network.Query(1, 6)); // True
+            Console.WriteLine(network.Query(6, 1)); // True
+            Console.WriteLine(network.Query(6, 4)); // True
+            Console.WriteLine(network.Query(7, 4)); // False
+            Console.WriteLine(network.Query(5, 6)); // False
+        }
+    }
+}
